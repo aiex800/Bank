9617d69757457f100cabba49a1f57f19da9fe0f0
Alexander Häggström <alexander.haggstrom@learnet.se>
2019-10-08 10:57:38 +0200

    ny bank

--- bank.py
+++ bank.py
@@ -1,37 +1,42 @@
-registernamn = input("registrera användarnamn: ")
-registerlösen = input("registrera lösenord: ")
+registernamn = input("registrera användarnamn: ") 
+registerlösen = input("registrera lösenord: ") #man registrerar sitt lösenord har och användar namn ovanför
 
-file = open("balance.txt", "w+")
-saldo = file.read()
-print(saldo)
+file = open("balance.txt", "r+") #öppnar saldo dokumentet
+saldo = float(file.read())
+def removeContent():
+    file.seek(0)
+    file.truncate(0)
+    return file
+#saldo = float(input("hur mycket pengar har du?: "))
 
-saldo = float(input("hur mycket pengar har du?: "))
+#file.write(str(saldo))
 
-file.write(str(saldo))
-
-file.close()
+#file.close()
 
 
-while True:
-    namn = input("användarnamn: ")
-    lösen = input("lösenord: ")
-    if namn != registernamn:
+while True: #while loop
+    namn = input("användarnamn: ") 
+    lösen = input("lösenord: ") #skriver in namn och lösen
+    if namn != registernamn: #om namn är inte samma som registernamn går den inte vidare och skriver att det var fel
         print("Fel användarnamn")
         
-    elif namn == registernamn:
+    elif namn == registernamn: #kollar om det är rätt namn som är skrivet
         print(" ")
 
-    if lösen != registerlösen:
-        print("fel lösenord: ")
-
-    elif lösen != registerlösen + namn != registernamn :
-        print("hej " + namn)
-        print("ditt saldo är: " + str(saldo))
-        insättning = input("vill du ta ut eller sätta in pengar: ")
-        mängd = float(input("Hur mycket? "))
-        if insättning == "sätta in":
-            saldo += mängd
-            print("nya saldot är: " + str(saldo))
-        elif insättning == "ta ut":
-            saldo -= mängd
-            print("nya saldot är: " + str(saldo))
+        if lösen != registerlösen: #kollar om det är fel lösenord
+            print("fel lösenord: ") 
+        
+        elif lösen == registerlösen:
+            print("hej " + namn)
+            print("ditt saldo är: " + str(saldo))
+            insättning = input("vill du ta ut eller sätta in pengar: ")
+            mängd = float(input("Hur mycket? "))
+            #file.write
+            if insättning == "sätta in":
+                saldo = mängd + float(saldo)
+                removeContent()
+                file.write(str(saldo))
+                print("nya saldot är: " + str(saldo))
+            elif insättning == "ta ut":
+                saldo -= mängd
+                print("nya saldot är: " + str(saldo))
